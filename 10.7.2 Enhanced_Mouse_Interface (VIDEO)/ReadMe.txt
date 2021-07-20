e4ced84b94fe8ecfae0ac39f785dcabe57e1044d
AngeloJacobo <87559347+AngeloJacobo@users.noreply.github.com>
2021-07-20 11:32:19 +0800

    Add files via upload

---
+++ 10.7.2 Enhanced_Mouse_Interface (VIDEO)/ReadMe.txt
@@ -0,0 +1,23 @@
+Created by: Angelo Jacobo 
+Date: May 10,2021
+
+Inside the src folder are:
+mouse_test.v -> Uses the mouse module to make the leds move based on the x-axis movement of the ps2 mouse
+				Press key0 to transmit the code for enabling stream mode of the mouse
+				Press key1 to reset and disable the stream mode of he mouse
+mouse.v -> Module for ps/2 mouse interfacing.
+mouse_test.ucf -> Constraint file for mouse_test.v
+
+Note: The constraint file is designed for Spartan 6 xc6slx9-2ftg256 FPGA (specifically the AX309 FPGA development board). Edit at your own risk.
+
+
+
+TASK:
+10.7.2 Enhanced mouse interface
+
+For the mouse interface discussed in Section 10.5, we can alter the design to manually
+enable or disable the steam mode. This can be done by using two pushbuttons of the FPGA
+prototyping board. One button issues the reset command, FF, which disables the stream
+mode during operation, and the other button issues the F4 command to enable the steam
+mode. Modify the original interface to incorporate this feature, and resynthesize the LED
+testing circuit to verify its operation. 
---
+++ 10.7.2 Enhanced_Mouse_Interface (VIDEO)/src/ps2_rx.v
@@ -0,0 +1,75 @@
+`timescale 1ns / 1ps
+
+module ps2_rx(
+	input clk,rst_n,
+	input rx_en,
+	input ps2d,ps2c,
+	output[10:0] dout, //startbit,8databits,paritybit,stopbit
+	output reg rx_done_tick
+    );
+	 //FSM state declarations
+	 localparam[1:0] idle=2'd0,
+							scan=2'd1,
+							done=2'd2;
+	 reg[1:0] state_reg,state_nxt;
+	 reg[10:0] data_reg,data_nxt;
+	 reg[3:0] n_reg,n_nxt;
+	 reg[7:0] filter_reg,filter_nxt;
+	 reg ps2c_f_reg,ps2c_f_nxt;
+	 reg ps2c_reg;
+	 wire ps2c_edg;
+	 //FSM register operations
+	 always @(posedge clk,negedge rst_n) begin
+		if(!rst_n) begin
+			state_reg<=0;
+			data_reg<=0;
+			n_reg<=0;
+			filter_reg<=0;
+			ps2c_f_reg<=0;
+			ps2c_reg<=0;
+		end
+		else begin
+			state_reg<=state_nxt;
+			data_reg<=data_nxt;
+			n_reg<=n_nxt;
+			filter_reg<=filter_nxt;
+			ps2c_f_reg<=ps2c_f_nxt;
+			ps2c_reg<=ps2c_f_reg;
+		end
+	 end
+	 
+	 //FSM next-state logic
+	 always @* begin
+		state_nxt=state_reg;
+		data_nxt=data_reg;
+		n_nxt=n_reg;
+		rx_done_tick=0;
+		case(state_reg)
+			   idle: if(ps2c_edg && rx_en) begin //wait for the falling edge of the start bit
+							data_nxt={ps2d,data_reg[10:1]};
+							n_nxt=0;
+							state_nxt=scan;
+						end						
+			   scan: if(ps2c_edg) begin //wait to finish receiving all data bits(startbit,bit0-7,paritybit,then stopbit)
+							data_nxt={ps2d,data_reg[10:1]};
+							if(n_reg==9) state_nxt=done;
+							else n_nxt=n_reg+1;
+						end
+			   done: begin //finish
+							rx_done_tick=1;
+							state_nxt=idle;
+						end
+			default: state_nxt=idle;	
+		endcase
+	 end
+	 //ps2c filter logic. The ps2c_f_reg is the filtered output.
+	 always @* begin 
+		filter_nxt={ps2c,filter_reg[7:1]}; 
+		if(filter_reg==8'b1111_1111) ps2c_f_nxt=1;  //Any glitches shorter than 8 turns will be ignored
+		else if(filter_reg==8'b0000_0000) ps2c_f_nxt=0; 
+		else ps2c_f_nxt=ps2c_f_reg;
+	 end
+	 //falling edge detection for ps2c_f_reg
+	 assign ps2c_edg=(ps2c_reg && !ps2c_f_reg)?1:0;
+	assign dout=data_reg;
+endmodule
